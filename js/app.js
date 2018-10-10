987da6095878f367d8bded954785c6943aeaa284
Karen Kepner <kkepner14@gmail.com>
2018-10-10 11:57:39 -0600

    Add move counter, repair compareCards, add reset game

--- js/app.js
+++ js/app.js
@@ -13,8 +13,11 @@ let cardPicture = [ 'fa-bicycle', 'fa-bicycle',
 let deck = document.querySelector(".deck");
 let cards = [];
 let clickedCards = [];
+let clicks = 0;
 let moves = 0;
 let movesCount = document.querySelector('.moves');
+let matches = 0;
+let gameResetButton = document.querySelector('.restart').addEventListener('click', resetGame);
 
 function createCards(card) {
   return `<li class="card" data-card=${card}><i class="fa ${card}"></i></li>`
@@ -28,42 +31,59 @@ function startGame() {
   cards = Array.from(document.querySelectorAll(".card"));
   console.log(cards)
   makeClickable(cards);
-  moves = 0;
- }
+  clicks = 0;
+}
 
 function makeClickable(card) {
   cards.forEach(function(card){
     card.addEventListener('click', function(){
-        if (clickedCards.length === 2) {
-          //compare cards.
-          if (clickedCards[0].dataset.card === clickedCards[1].dataset.card) {
-            clickedCards[0].classList.add('match');
-            clickedCards[1].classList.add('match');
-            console.log(card.dataset.card);
-            clickedCards = [];
-          } else {
-            //setTimeout(function(){
-            clickedCards[0].classList.remove('open', 'show');
-            clickedCards[1].classList.remove('open', 'show');
-            //}, 1000)
-            clickedCards = [];
-          };
-        //show cards
-        } else {
-          if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-              card.classList.add('open', 'show');
-              clickedCards.push(card);
-              console.log('clickedCards length: ' + clickedCards.length + " and "+ clickedCards[0].dataset.card);
-          }
-      };
-      moves++;
+      if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match') && clickedCards.length<2) {
+          card.classList.add('open', 'show');
+          clickedCards.push(card);
+          console.log("card pushed to clickedCards");
+          console.log("array length: " + clickedCards.length);
+          moves++;
+          console.log("moves clicked: " + moves);
+      }
+      compareCards();
       movesCount.innerText = moves;
-      console.log(moves);
     })
+    //didPlayerWinYet();
+    //displayMoves(clicks);
   })
 }
 
+function compareCards(){
+  if (clickedCards.length === 2){
+    if (clickedCards[0].dataset.card === clickedCards[1].dataset.card) {
+      clickedCards[0].classList.add('match');
+      clickedCards[1].classList.add('match');
+      matches++;
+      console.log('Matches= ' + matches);
+      console.log('cards compared');
+      didPlayerWinYet();
+      clickedCards = [];
+    } else {
+      setTimeout(function(){
+        clickedCards[0].classList.remove('open', 'show');
+        clickedCards[1].classList.remove('open', 'show');
+        clickedCards = [];
+      }, 1000)
+    }
+  }
+}
 
+function didPlayerWinYet(){
+  if (matches === 8) {
+    //alert("You Win!" + "Moves = " + moves + " Stars = " +);
+    alert(`You win! Moves Taken: ${moves} Star Rating: unknown`)
+    //startGame();
+  }
+}
+
+function displayMoves(click) {
+ moves = click/2;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -79,5 +99,8 @@ function shuffle(array) {
     return array;
 }
 
+function resetGame(){
+  startGame();
+ }
 
 startGame();
